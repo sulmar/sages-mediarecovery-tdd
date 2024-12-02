ed44365f0577736c4b99b301a095f624838a5d94
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-12-02 11:57:38 +0100

    Refactored

--- src/TestApp/TDD/DiscountCalculator.cs
+++ src/TestApp/TDD/DiscountCalculator.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 
 namespace TestApp.TDD;
 
+public class DiscountCodeParams
+{
+    public string DiscountCode { get; set; }
+    public decimal Percentage { get; set; }
+    public bool IsReusable { get; set; }
+    public bool IsActive { get; set; } = true;
+
+}
+
 public class DiscountCalculator
 {
-    private readonly Dictionary<string, bool> _discountCodes = [];
+    private readonly Dictionary<string, DiscountCodeParams> _discountCodes = [];
 
     public DiscountCalculator()
     {
-        _discountCodes.Add("XYZ", true); // discont_code, isActive
+        var discountCodeParams1 = new DiscountCodeParams { DiscountCode = "XYZ", Percentage = 0.5M};
+        var discountCodeParams2 = new DiscountCodeParams { DiscountCode = "SAVE10NOW", Percentage = 0.9M, IsReusable = true};
+        var discountCodeParams3 = new DiscountCodeParams { DiscountCode = "DISCOUNT20OFF", Percentage = 0.8M, IsReusable = true};
+        
+        _discountCodes.Add(discountCodeParams1.DiscountCode, discountCodeParams1); // discont_code, isActive
+        _discountCodes.Add(discountCodeParams2.DiscountCode, discountCodeParams2); // discont_code, isActive
+        _discountCodes.Add(discountCodeParams3.DiscountCode, discountCodeParams3); // discont_code, isActive
     }
     
     public decimal CalculateDiscount(decimal price, string discountCode)
@@ -20,23 +35,19 @@ public class DiscountCalculator
         if (string.IsNullOrEmpty(discountCode))
             return price;
         
-        if (discountCode == "SAVE10NOW")
-            return price * 0.9M;
-        
-        if (discountCode == "DISCOUNT20OFF")
-            return price * 0.8M;
-
         if (_discountCodes.ContainsKey(discountCode))
         {
-            if (_discountCodes[discountCode])
+            var discountCodeParams = _discountCodes[discountCode];
+            
+            if (discountCodeParams.IsActive)
             {
-                _discountCodes[discountCode] = false;
-                return price * 0.5M;
+                discountCodeParams.IsActive = discountCodeParams.IsReusable;
+
+                return price * discountCodeParams.Percentage;
             }
-            else
-            {
-                throw new ArgumentException("Discount code has been used");
-            }
+            
+            throw new ArgumentException("Discount code has been used");
+            
         }
         
         throw new ArgumentException("Invalid discount code");
