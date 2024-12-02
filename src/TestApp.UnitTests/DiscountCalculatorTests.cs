34ebdad59409215300e0fb7ed17c78dab549f680
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2024-12-02 11:46:25 +0100

    Dodanie kalkulatora rabatów

---
+++ src/TestApp.UnitTests/DiscountCalculatorTests.cs
@@ -0,0 +1,123 @@
+﻿using System.Net.NetworkInformation;
+using TestApp.TDD;
+
+namespace TestApp.UnitTests;
+
+[TestClass]
+public class DiscountCalculatorTests
+{
+    // W przypadku podania pustego kodu rabat nie będzie udzielany.
+    
+    // Method_Scenario_ExpectedBehavior
+    
+    private const decimal OriginalPrice = 100.00M;
+    private const decimal NegativePrice = -1.00M; 
+    private const string InvalidDiscountCode = "a";
+    private const string SingleUseDiscountCode = "XYZ"; 
+
+    // 1. W przypadku podania pustego kodu rabat nie będzie udzielany.
+    [TestMethod]
+    public void CalculateDiscount_WhenDiscountCodeIsEmpty_ShouldReturnPrice()
+    {
+        // Arrange
+        DiscountCalculator discountCalculator = new DiscountCalculator();
+
+        // Act
+        var result = discountCalculator.CalculateDiscount(OriginalPrice, string.Empty);
+        
+        // Assert
+        Assert.AreEqual(100, result);
+    }
+    
+    // 2. Dodaj rabat 10%, który będzie naliczany po podaniu kodu kuponu SAVE10NOW.
+    [TestMethod]
+    public void CalculateDiscount_WhenDiscountCodeIsSAVE10NOW_ShouldReturnDiscountedPriceBy10Percent()
+    {
+        // Arrange
+        DiscountCalculator discountCalculator = new DiscountCalculator();
+        
+        // Act
+        var result = discountCalculator.CalculateDiscount(OriginalPrice, "SAVE10NOW");
+        
+        // Assert
+        Assert.AreEqual(90.00M, result);
+    }
+    
+    // 3. Dodaj rabat 20%, który będzie naliczany po podaniu kodu kuponu DISCOUNT20OFF.
+    [TestMethod]
+    public void CalculateDiscount_WhenDiscountCodeIsDISCOUNT20OFF_ShouldReturnDiscountedPriceBy20Percent()
+    {
+        // Arrange
+        DiscountCalculator discountCalculator = new DiscountCalculator();
+        
+        // Act
+        var result = discountCalculator.CalculateDiscount(OriginalPrice, "DISCOUNT20OFF");
+        
+        // Assert
+        Assert.AreEqual(80.00M, result);
+    }
+    
+    // 4. Wywołanie metody CalculateDiscount z ujemną ceną powinno rzucić wyjątkiem ArgumentException z komunikatem "Negatives not allowed".
+
+    [TestMethod]
+    public void CalculateDiscount_WhenNegativePrice_ShouldThrowArgumentExceptionWithMessageNegativesNotAllowed()
+    {
+        // Arrange
+        DiscountCalculator discountCalculator = new DiscountCalculator();
+        
+        // Act
+        Action act = () => discountCalculator.CalculateDiscount(NegativePrice, string.Empty);
+        
+        // Assert
+        var result = Assert.ThrowsException<ArgumentException>(act);
+        Assert.AreEqual("Negatives not allowed", result.Message);
+
+    }
+    
+    // 5. W przypadku błędnego kodu powinien być zwracany wyjątek ArgumentException z komunikatem "Invalid discount code"
+    [TestMethod]
+    public void CalculateDiscount_WhenInvalidDiscountCode_ShouldThrowArgumentExceptionWithMessageInvalidDiscountCode()
+    {
+        // Arrange
+        DiscountCalculator discountCalculator = new DiscountCalculator();
+        
+        // Act
+        Action act = () => discountCalculator.CalculateDiscount(OriginalPrice, InvalidDiscountCode);
+        
+        // Assert
+        var result = Assert.ThrowsException<ArgumentException>(act);
+        Assert.AreEqual("Invalid discount code", result.Message);
+    }
+    
+    // 6. Dodaj rabat 50%, który jest naliczany jednorazowo na podstawie kodu z puli kodów.
+
+    [TestMethod]
+    public void CalculateDiscount_WhenFirstUseDiscountCodeInPool_ShouldReturnDiscountedPriceBy50Percent()
+    {
+        // Arrange
+        DiscountCalculator discountCalculator = new DiscountCalculator();
+        
+        // Act
+        var result = discountCalculator.CalculateDiscount(OriginalPrice, SingleUseDiscountCode);
+        
+        // Assert
+        Assert.AreEqual(50.00M, result);
+        
+    }
+    
+    [TestMethod]
+    public void CalculateDiscount_WhenNextUseDiscountCodeInPool_ShouldThrowArgumentExceptionWithMessageDiscountCodeHasBeenUsed()
+    {
+        // Arrange
+        DiscountCalculator discountCalculator = new DiscountCalculator();
+        discountCalculator.CalculateDiscount(OriginalPrice, SingleUseDiscountCode);
+        
+        // Act
+        Action act = () => discountCalculator.CalculateDiscount(OriginalPrice, SingleUseDiscountCode);  
+        
+        // Assert
+        var result = Assert.ThrowsException<ArgumentException>(act);
+        Assert.AreEqual("Discount code has been used", result.Message);
+        
+    }
+}
---
+++ src/TestApp/TDD/DiscountCalculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestApp.TDD;
+
+public class DiscountCalculator
+{
+    private readonly Dictionary<string, bool> _discountCodes = [];
+
+    public DiscountCalculator()
+    {
+        _discountCodes.Add("XYZ", true); // discont_code, isActive
+    }
+    
+    public decimal CalculateDiscount(decimal price, string discountCode)
+    {
+        if (price < 0)
+            throw new ArgumentException("Negatives not allowed");
+        
+        if (string.IsNullOrEmpty(discountCode))
+            return price;
+        
+        if (discountCode == "SAVE10NOW")
+            return price * 0.9M;
+        
+        if (discountCode == "DISCOUNT20OFF")
+            return price * 0.8M;
+
+        if (_discountCodes.ContainsKey(discountCode))
+        {
+            if (_discountCodes[discountCode])
+            {
+                _discountCodes[discountCode] = false;
+                return price * 0.5M;
+            }
+            else
+            {
+                throw new ArgumentException("Discount code has been used");
+            }
+        }
+        
+        throw new ArgumentException("Invalid discount code");
+    }
+}
